f27e68fb738803dea59e49e394a38720a6449690
Manuel Spuhler <manuel.spuhler@gmail.com>
2016-01-24 01:02:19 +0100

    Device Orientation API controller

--- main.js
+++ main.js
@@ -1,42 +1,66 @@
 (function () {
-  console.log("Starting");
+    console.log("Starting");
 
-  $(document).on("keydown", function (e) {
+    function process(event) {
+      var alpha = event.alpha;
+      var beta = event.beta;
+      var gamma = event.gamma
+      console.log("alpha: " + alpha + "beta: " + beta + "gamma: " + gamma);
 
-    switch (e.keyCode) {
-    case 38:
-      post("north");
-      break;
+      if (alpha > 45 && alpha <= 315) post("north");
+      if (alpha > 315 && alpha <= 225) post("east");
+      if (alpha > 225 && alpha <= 135) post("south");
+      if (alpha > 135 && alpha <= 0) post("west");
 
-    case 40:
-      post("south");
-      break;
+      if (beta > 0) post("fast");
+      if (beta < 0) post("slow");
 
-    case 37:
-      post("west");
-      break;
-
-    case 39:
-      post("east");
-      break;
-
-    case 87:
-      post("fast");
-      break;
-
-    case 83:
-      post("slow");
-      break;
-
-    case 65:
-      post("left");
-      break;
-
-    case 68:
-      post("right");
-      break;
+      if (gamma > 0) post("right");
+      if (gamma < 0) post("left");
     }
-  });
+
+    if (window.DeviceOrientationEvent) {
+      window.addEventListener("deviceorientation", process, false);
+    } else {
+      console.log("wowwowow deviceorientation not supported.");
+          }
+
+    $(document).on("keydown", function (e) {
+
+      switch (e.keyCode) {
+      case 38:
+        post("north");
+        break;
+
+      case 40:
+        post("south");
+        break;
+
+      case 37:
+        post("west");
+        break;
+
+      case 39:
+        post("east");
+        break;
+
+      case 87:
+        post("fast");
+        break;
+
+      case 83:
+        post("slow");
+        break;
+
+      case 65:
+        post("left");
+        break;
+
+      case 68:
+        post("right");
+        break;
+      }
+    });
 
   $(".north").on("click", function () {
     post("north");
@@ -71,23 +95,24 @@
   });
 
   function post(data) {
+    console.log("post: " + data)
     //var url = "http://bb8.oviles.info/api/send";
     var url = "/api/send";
-    var data2 = {
+    var msg = {
       "created": new Date(),
       "msg": data
     };
-    console.log("Sending: " + data)
+    console.log("Sending msg: " + data);
 
     $.ajax({
       type: "POST",
       crossDomain: true,
       url: url,
-      data: JSON.stringify(data2),
+      data: JSON.stringify(msg),
       success: function () {
-        console.log("Successfully sent" + data2)
+        console.log("Successfully sent" + msg)
       },
-      dataType: "json"
+      dataType: "application/json"
     });
   }
 
